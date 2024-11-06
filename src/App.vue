e5298b3af9d227ffdf0b21a89115261b810ed2ce
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-11-06 13:41:47 +0700

    feat: init app

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,58 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import { COL_LENGTH, DOWN, LEFT, RIGHT, ROW_LENGTH, UP } from '@/constants'
+import type { Direction, RowCol, SnakeBody } from '@/types'
+import Snake from '@/classes/snake'
+
+const grid: number[][] = new Array(ROW_LENGTH).fill(new Array(COL_LENGTH).fill(0))
+
+const oppositeDirection = {
+  [RIGHT]: LEFT,
+  [LEFT]: RIGHT,
+  [UP]: DOWN,
+  [DOWN]: UP,
+}
+
+const generateFood = (body: SnakeBody) => {
+  const rowCols: RowCol[] = []
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      const rowCol: RowCol = [i, j]
+      if (!body[JSON.stringify([i, j])]) {
+        rowCols.push(rowCol)
+      }
+    }
+  }
+  const randomIndex = Math.floor(Math.random() * rowCols.length)
+  return rowCols[randomIndex]
+}
+
+const snake = ref(new Snake())
+const snakeBody = ref(snake.value.body)
+const snakeLength = ref(snake.value.length)
+const food = ref<RowCol>(generateFood(snakeBody.value))
+const direction = ref<Direction>(snake.value.currentDirection)
+const intervalId = ref<NodeJS.Timeout | null>(null)
+const isGameOver = ref(false)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="flex flex-col items-center w-full">
+    <p class="text-sm mb-6">
+      Press arrow keys (↑, →, ↓, ←) to change direction. Press Enter or Space to start and pause.
+    </p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="py-2 text-sm">Length:</div>
+    <div class="relative flex flex-col w-full lg:w-[unset]">
+      <div
+        class="absolute left-0 right-0 bottom-0 top-0 z-9 bg-black bg-opacity-75 flex flex-col items-center justify-center cursor-pointer"
+      >
+        <div class="absolute top-[30px] flex flex-col items-center">
+          <div class="text-white text-3xl mb-1">Click to play!</div>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
